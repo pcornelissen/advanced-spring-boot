4755bf2af09454cd9adf11eb443f1ad60eb6e42c
Patrick Cornelissen <cornelis@pcornelissen.de>
2017-09-17 20:35:28 +0200

    Section 3.3 UI Testing with HtmlUnit

--- src/test/java/com/packtpub/yummy/ui/UiControllerTest.java
+++ src/test/java/com/packtpub/yummy/ui/UiControllerTest.java
@@ -1,5 +1,10 @@
 package com.packtpub.yummy.ui;
 
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.packtpub.yummy.model.Bookmark;
 import com.packtpub.yummy.rest.BookmarksController;
 import com.packtpub.yummy.service.BookmarkService;
@@ -22,6 +27,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -38,6 +45,9 @@ public class UiControllerTest {
     );
 
     @Autowired
+    WebClient wc;
+
+    @Autowired
     MockMvc mvc;
 
     @MockBean
@@ -46,6 +56,44 @@ public class UiControllerTest {
     @Before
     public void before() {
         when(bookmarkService.findAll()).thenReturn(bookmarks);
+    }
+
+    @Test
+    public void testFindAllViaHtmlUnit() throws Exception {
+// You can for example create a test that loads a page and checks for errors or errors and warnings. Of
+// course it would make sense to collect all errors and fail later if there is more than one,
+// so you can fix all errors at once and the run the test again. Currently our page contains html errors, so I disabled it.
+//        wc.setHTMLParserListener(new HTMLParserListener() {
+//            @Override
+//            public void error(String message, URL url, String html, int line, int column, String key) {
+//                fail("Error: "+message+" line: "+line+" column :"+column);
+//            }
+//
+//            @Override
+//            public void warning(String message, URL url, String html, int line, int column, String key) {
+//                fail("Warning: "+message+" line: "+line+" column :"+column);
+//            }
+//        });
+
+        String s = wc.getPage("/").getWebResponse().getContentAsString();
+        assertThat(s, containsString("orchit.de"));
+
+        Page page= wc.getPage("/");
+        assertTrue(page.isHtmlPage());
+        HtmlPage html= (HtmlPage)page;
+
+        HtmlTable table = html.getBody().getFirstByXPath("//table");
+        assertNotNull(table);
+        assertEquals(bookmarks.size(), table.getRowCount());
+
+        HtmlButton btn = html.getBody().getFirstByXPath("//table//form/button");
+        assertNotNull(btn);
+        btn.click();
+
+        HtmlTable table2 =html.getBody().querySelector("table");
+        assertNotNull(table2);
+        assertEquals(bookmarks.size(), table2.getRowCount());
+
     }
 
     @Test
